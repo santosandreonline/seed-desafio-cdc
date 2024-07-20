a4911c874df7ed7a203bb98cc5fb9c803f5d4cc6
santosandreonline <santos.andreonline@gmail.com>
2024-07-20 01:03:34 -0300

    commit inicial
    java + spring via spring boot

---
+++ src/main/java/com/andresantos/seed_desafio_cdc/SeedDesafioCdcApplication.java
@@ -0,0 +1,13 @@
+package com.andresantos.seed_desafio_cdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedDesafioCdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeedDesafioCdcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andresantos/seed_desafio_cdc/SeedDesafioCdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresantos.seed_desafio_cdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeedDesafioCdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
