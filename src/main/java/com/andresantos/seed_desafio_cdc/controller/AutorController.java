2dc789de2912dc82b76f2fe3801f12086b7263b7
santosandreonline <santos.andreonline@gmail.com>
2024-07-21 21:09:35 -0300

    validation

--- src/main/java/com/andresantos/seed_desafio_cdc/controller/AutorController.java
+++ src/main/java/com/andresantos/seed_desafio_cdc/controller/AutorController.java
@@ -1,17 +1,18 @@
 package com.andresantos.seed_desafio_cdc.controller;
 
 import com.andresantos.seed_desafio_cdc.dto.CadastrarNovoAutorDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 
 @RestController
 public class AutorController {
 
     @PostMapping("/autor")
-    public Object cadastrarNovoAutor(@RequestBody CadastrarNovoAutorDTO novoAutorDTO){
-        return novoAutorDTO;
+    public ResponseEntity<CadastrarNovoAutorDTO> cadastrarNovoAutor(@Valid @RequestBody CadastrarNovoAutorDTO novoAutorDTO){
+        return ResponseEntity.ok(novoAutorDTO);
     }
 }
--- src/main/java/com/andresantos/seed_desafio_cdc/dto/CadastrarNovoAutorDTO.java
+++ src/main/java/com/andresantos/seed_desafio_cdc/dto/CadastrarNovoAutorDTO.java
@@ -1,10 +1,32 @@
 package com.andresantos.seed_desafio_cdc.dto;
 
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class CadastrarNovoAutorDTO {
 
+    @NotNull(message = "Campo nome não pode ser nulo")
+    @NotBlank(message = "Campo nome não pode ser vazio")
     private String nome;
+    @NotNull
+    @NotBlank
+    @Email
     private String email;
+    @NotNull
+    @NotBlank
+    @Size(min = 1, max = 400)
     private String descricao;
+
+    @Override
+    public String toString() {
+        return "CadastrarNovoAutorDTO{" +
+                "nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 
     public void setNome(String nome) {
         this.nome = nome;
